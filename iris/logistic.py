f7ecf99cd044f1f38094f7bf125f66e94df3d9c5
jchang4 <justinchangmusic@gmail.com>
2018-08-18 16:43:55 -0700

    Remove unnecessary files, using conda instead of venv

--- iris/logistic.py
+++ iris/logistic.py
@@ -1,119 +1,71 @@
+import pandas as pd
 import numpy as np
+from keras.utils import to_categorical
 
-class Neural:
+"""
+    One vs Many Classifier
+"""
 
-    def initialize_with_zeros(self, dim):
-        w = np.zeros((dim, 1))
-        b = 0
-        return w, b
+train_data = pd.read_pickle('./data/train.pickle')
 
-    def sigmoid(self, z):
-        return 1 / (1 + np.exp(-z))
+# Randomize Data
+train_data = train_data.sample(frac=1).reset_index(drop=True)
 
-    def propogate(self, w, b, X, Y):
-        """
-        Calculate cost and gradient
-        """
-        m = X.shape[1]
-        z = np.dot(w.T, X) + b
-        A = self.sigmoid(z)
-        cost = -1/m * np.sum(np.multiply(Y, np.log(A)), np.multiply(1-Y, np.log(1-A)))
-
-        # Gradients
-        Y_diff = A - Y
-        dw = 1/m * np.dot(X, Y_diff.T)
-        db = 1/m * np.sum(Y_diff)
-
-        cost = np.squeeze(cost)
-        grads = {"dw": dw,
-                 "db": db}
-
-        return grads, cost
-
-    def optimize(self, w, b, X, Y, num_iterations, learning_rate, print_cost = False):
-
-        costs = []
-
-        for i in range(num_iterations):
-            grads, cost = self.propogate(w, b, X, Y)
-
-            dw = grads["dw"]
-            db = grads["db"]
-
-            w -= np.multiply(learning_rate, dw)
-            b -= np.multiply(learning_rate, db)
-
-            if i % 100 == 0:
-                costs.append(cost)
-
-            if print_cost and i % 100 == 0:
-                print ("Cost after iteration %i: %f" %(i, cost))
-
-            params = {"w": w,
-                      "b": b}
-
-            grads = {"dw": dw,
-                     "db": db}
-
-            return params, grads, costs
-
-    def predict(self, w, b, X):
-        m = X.shape[1]
-        Y_prediction = np.zeros((1,m))
-        w = w.reshape(X.shape[0], 1)
-
-        A = self.sigmoid(np.dot(w.T, X) + b)
-
-        for i in range(A.shape[1]):
-            Y_prediction[0][i] = 1 if A[0][i] >= 0.5 else 0
-
-        return Y_prediction
+def setup_data(data):
+    X = data[['PetalLengthCm', 'PetalWidthCm']].values.T
+    Y = data[['Species']].values.T
+    Y = to_categorical(Y, num_classes=3)
+    return X, Y
 
 
-    def model(self, X_train, Y_train, X_test, Y_test, num_iterations=2000, learning_rate=0.001, print_cost = False):
+def sigmoid(x):
+    return 1 / (1 + np.exp(-x))
 
-        w, b = self.initialize_with_zeros(X_train.shape[0])
-        parameters, grads, costs = self.optimize(w, b, X_train, Y_train, num_iterations, learning_rate, print_cost)
+def initialize_weights(X, Y):
+    num_features = X.shape[0]
+    num_labels = Y.shape[0]
+    weights = np.random.randn(num_labels, num_features)
+    biases = np.zeros(num_labels)
+    return weights, biases
 
-        w = parameters["w"]
-        b = parameters["b"]
-
-        Y_prediction_test = self.predict(w, b, X_test)
-        Y_prediction_train = self.predict(w, b, X_train)
-
-        print("train accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_train - Y_train)) * 100))
-        print("test accuracy: {} %".format(100 - np.mean(np.abs(Y_prediction_test - Y_test)) * 100))
-
-
-        d = {"costs": costs,
-             "Y_prediction_test": Y_prediction_test,
-             "Y_prediction_train" : Y_prediction_train,
-             "w" : w,
-             "b" : b,
-             "learning_rate" : learning_rate,
-             "num_iterations": num_iterations}
-
-        return d
+def forward(weights, X):
+    """
+    Cost function:
+        -ylog(yhat) + (1-y)log(1-yhat)
+    """
+    # yhat =
+    pass
 
 
-if __name__ == '__main__':
-    import pandas as pd
+def calc_cost(y, yhat):
+    logsum = np.dot(-y, np.log(yhat)) + np.dot(1-y, np.log(1-yhat))
+    return -1/m * logsum
 
-    train_data = pd.read_pickle('./data/train.pickle')
-    valid_data = pd.read_pickle('./data/valid.pickle')
+def backward():
+    pass
 
-    # Split Data into Features & Labels
-    X_train = train_data[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']].values.T
-    Y_train = train_data[['Species']].values.T
+def update_weights():
+    pass
 
-    X_valid = valid_data[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']].values.T
-    Y_valid = valid_data[['Species']].values.T
+def train(X, Y, learning_rate=0.01, num_iterations=100):
+    weights = initialize_weights(X, Y)
+    for i in range(num_iterations):
+        forward()
+        calc_cost()
+        backward()
+        weights = update_weights()
+    return weights
 
-    print(X_train[:][1:5])
-    print(Y_train[:][1:5])
-    print(X_train.shape)
-    print(Y_train.shape)
-    print()
+def predict():
+    pass
 
-    clf = Neural()
-    d = clf.model(X_train, Y_train, X_valid, Y_valid, print_cost = True)
+def accuracy():
+    pass
+
+
+
+
+X_train, Y_train = setup_data(train_data)
+
+print(X_train[:][:1])
+print(Y_train[:][:1])
--- iris/quick_nn.py
+++ iris/quick_nn.py
@@ -8,25 +8,25 @@ def accuracy(y_pred, y):
     return np.sum(y_pred == y) / y.shape[0] * 100
 
 train_data = pd.read_pickle('./data/train.pickle')
-X_train = train_data[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']].values
+X_train = train_data[['PetalLengthCm', 'PetalWidthCm']].values
 Y_train = train_data[['Species']].values
 
 
 valid_data = pd.read_pickle('./data/valid.pickle')
-X_valid = valid_data[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']].values
+X_valid = valid_data[['PetalLengthCm', 'PetalWidthCm']].values
 Y_valid = valid_data[['Species']].values
 
 test_data = pd.read_pickle('./data/test.pickle')
-X_test = test_data[['SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm']].values
+X_test = test_data[['PetalLengthCm', 'PetalWidthCm']].values
 Y_test = test_data[['Species']].values
 
 
 
 model = Sequential()
-model.add(Dense(64, activation='relu', input_dim=X_train.shape[1]))
+model.add(Dense(128, activation='relu', input_dim=X_train.shape[1]))
 model.add(Dense(64, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(128, activation='relu'))
+model.add(Dense(16, activation='relu'))
+# model.add(Dense(128, activation='relu'))
 model.add(Dense(3, activation='softmax'))
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
@@ -36,15 +36,15 @@ model.compile(optimizer='rmsprop',
 one_hot_labels = keras.utils.to_categorical(Y_train, num_classes=3)
 
 # Train the model, iterating on the data in batches of 32 samples
-model.fit(X_train, one_hot_labels, epochs=10, batch_size=32)
+model.fit(X_train, one_hot_labels, epochs=275, batch_size=32)
 score = model.evaluate(X_valid, keras.utils.to_categorical(Y_valid, num_classes=3), batch_size=128)
 
-print(score)
+print('Tranining Score:', score)
 
 y_pred = np.argmax(model.predict(X_valid), axis=1)
 y_pred = y_pred.reshape(Y_valid.shape[0], 1)
-print('Accuracy:', accuracy(y_pred, Y_valid), '%')
+print('Valid Accuracy:', accuracy(y_pred, Y_valid), '%')
 
-y_pred = np.argmax(model.predict(X_test), axis=1)
-y_pred = y_pred.reshape(Y_test.shape[0], 1)
-print('Accuracy:', accuracy(y_pred, Y_test), '%')
+# y_pred = np.argmax(model.predict(X_test), axis=1)
+# y_pred = y_pred.reshape(Y_test.shape[0], 1)
+# print('Test Accuracy:', accuracy(y_pred, Y_test), '%')
