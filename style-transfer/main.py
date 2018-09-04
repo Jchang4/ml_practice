a5a07622a680f563e048c58a95a729860422591c
jchang4 <justinchangmusic@gmail.com>
2018-09-03 22:08:13 -0700

    Move around files and clean up getting closer to transfer learning

--- style-transfer/main.py
+++ style-transfer/main.py
@@ -1,38 +1,14 @@
 import tensorflow as tf
-import keras
-from keras.applications.vgg19 import VGG19
+import matplotlib.pyplot as plt
+from keras.preprocessing.image import load_img
+from keras.applications.vgg19 import decode_predictions
+# from keras.applications.vgg19 import preprocess_input
 
+from scripts.images import load_and_process_img, imshow
 from model import get_model
-from images import load_and_process_img
 
 
+path_to_img = './data/sandwhich.jpeg'
 
-if __name__ == '__main__':
-
-
-    # Content layer where will pull our feature maps
-    content_layers = ['block5_conv2']
-
-    # Style layer we are interested in
-    style_layers = ['block1_conv1',
-                    'block2_conv1',
-                    'block3_conv1',
-                    'block4_conv1',
-                    'block5_conv1']
-
-    CONTENT_IMAGE_PATH = './data/turtle.png'
-    img = load_and_process_img(CONTENT_IMAGE_PATH)
-    print(img.shape)
-
-    # m = get_model(content_layers, style_layers)
-    # model = get_model(content_layers, style_layers)
-    # # sess = tf.Session()
-    # # output = sess.run(vgg, feed_dict={'input': img})
-    # output = model.predict(img)
-    #
-    # print(model)
-    # print(output.shape)
-
-
-    model = VGG19()
-    print(model.summary())
+image = load_and_process_img(path_to_img)
+model = get_model()
--- style-transfer/model.py
+++ style-transfer/model.py
@@ -3,7 +3,15 @@ import keras
 from keras.models import Model
 from keras.applications.vgg19 import VGG19
 
-def get_model(content_layers, style_layers):
+DEFAULT_CONTENT_LAYERS = ['block5_conv2']
+DEFAULT_STYLE_LAYERS = ['block1_conv1',
+                        'block2_conv1',
+                        'block3_conv1',
+                        'block4_conv1',
+                        'block5_conv1']
+
+
+def get_model(content_layers=DEFAULT_CONTENT_LAYERS, style_layers=DEFAULT_STYLE_LAYERS):
     """
     Take an input image and output content + style layers
     """
@@ -15,15 +23,19 @@ def get_model(content_layers, style_layers):
     style_output = [vgg.get_layer(layer).output for layer in style_layers]
     model_outputs = content_output + style_output
     # build model
-    # return Model(inputs=vgg.input, outputs=model_outputs)
-    return Model(inputs=vgg.input, outputs=vgg.output)
+    print(model_outputs)
+    return Model(inputs=vgg.input, outputs=model_outputs)
 
+
+# Content Loss
 def get_content_loss(base, target):
     """
         Content Loss = Sum (base - target)^2
     """
     return tf.reduce_sum(tf.square(base - target))
 
+
+# Style Loss
 def gram_matrix(A):
     return tf.matmul(A, tf.transpose(A))
 
@@ -37,6 +49,7 @@ def get_style_loss(base, target):
     return 1/(4 * n_C**2 * (n_H * n_W)**2) * loss_sum
 
 
-
-def get_loss():
-    pass
+def get_loss(content_base, content_target, style_base, style_loss):
+    J_content = get_content_loss(content_base, content_target)
+    J_style = get_style_loss(style_base, style_loss)
+    return J_content + J_style
