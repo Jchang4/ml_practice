f5ceab75adc03dee57fdb473e9b72dbf27d09860
jchang4 <justinchangmusic@gmail.com>
2018-09-09 13:08:35 -0700

    Working on neural style transfer using jupyter, much easier than what I was doing before

--- me-style/main.py
+++ me-style/main.py
@@ -6,6 +6,10 @@ from helpers.model import get_vgg_model, get_features_from_model
 
 from config import CONTENT_LAYER, STYLE_LAYERS
 
+""" Tensorflow + Keras """
+sess = tf.InteractiveSession()
+
+
 
 """ Get Images """
 content_path = './data/turtle.png'
@@ -13,3 +17,10 @@ style_path = './data/van-gogh.jpg'
 
 content_image = get_image(content_path)
 style_image = get_image(style_path)
+generated_image = generate_random_image()
+
+
+
+""" Get VGG19 """
+vgg = get_vgg_model()
+sess.run(vgg.)
