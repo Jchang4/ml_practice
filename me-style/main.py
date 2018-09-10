e7454f5a85768d17323b7c20ee112d73c2413978
jchang4 <justin.chang@buildingconnected.com>
2018-09-10 09:03:21 -0700

    Get rid of unused files/folders and move test.py to main.py

--- me-style/main.py
+++ me-style/main.py
@@ -1,26 +1,30 @@
 import tensorflow as tf
-import numpy as np
 
-from helpers.image import get_image, generate_random_image
-from helpers.model import get_vgg_model, get_features_from_model
+from helpers.image import get_image, save_image, generate_noise_image
+from helpers.model import get_model, nst_model
 
-from config import CONTENT_LAYER, STYLE_LAYERS
+from config import CONFIG
 
-""" Tensorflow + Keras """
-sess = tf.InteractiveSession()
+# Reset the graph
+tf.reset_default_graph()
+
+with tf.Session() as sess:
+
+# sess = tf.Session()
+
+    """ Get Images """
+    content_path = './data/seal.jpeg'
+    style_path = './data/cubism-1.jpg'
+
+    content_img = tf.constant(get_image(content_path, show=False, title='Content Image'), name='content_img', dtype=tf.float32)
+    style_img = tf.constant(get_image(style_path, show=False, title='Style` Image'), name='style_img', dtype=tf.float32)
+    generated_img = tf.Variable(generate_noise_image(content_path, show=False), name='generated_img', dtype=tf.float32)
+
+    """ Setup Model and Initial Costs """
+    model = get_model()
+
+    """ Train the Model! """
+    nst_model(sess, model, generated_img, content_img, style_img)
 
 
-
-""" Get Images """
-content_path = './data/turtle.png'
-style_path = './data/van-gogh.jpg'
-
-content_image = get_image(content_path)
-style_image = get_image(style_path)
-generated_image = generate_random_image()
-
-
-
-""" Get VGG19 """
-vgg = get_vgg_model()
-sess.run(vgg.)
+    # sess.close()
