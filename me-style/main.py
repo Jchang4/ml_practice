1635abd4c44731177c6c6a7302e032ed0b7b3a16
jchang4 <justin.chang@buildingconnected.com>
2018-09-12 15:16:47 -0700

    Change multi-line comments to regular comments

--- me-style/main.py
+++ me-style/main.py
@@ -9,7 +9,7 @@ from config import CONFIG
 tf.reset_default_graph()
 
 with tf.Session() as sess:
-    """ Get Images """
+    # Get Images
     content_path = './data/seal.jpeg'
     style_path = './data/cubism-1.jpg'
 
@@ -17,8 +17,8 @@ with tf.Session() as sess:
     style_img = tf.constant(get_image(style_path, show=False, title='Style` Image'), name='style_img', dtype=tf.float32)
     generated_img = tf.Variable(generate_noise_image(content_path, show=False), name='generated_img', dtype=tf.float32)
 
-    """ Setup Model and Initial Costs """
+    # Setup Model and Initial Costs
     model = get_model()
 
-    """ Train the Model! """
+    # Train the Model!
     nst_model(sess, model, generated_img, content_img, style_img)
