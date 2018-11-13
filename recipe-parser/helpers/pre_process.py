eb094f3a3dcd1798622b8c9bd17450b6d326183d
jchang4 <justinchangmusic@gmail.com>
2018-11-12 19:21:59 -0800

    Change fractions to decimals for consistent processing

--- recipe-parser/helpers/pre_process.py
+++ recipe-parser/helpers/pre_process.py
@@ -1,20 +1,31 @@
+import re
 import pandas as pd
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 
-def remove_stop_words(input_col):
-    stop_words = set(stopwords.words("english"))
-    
-    def remove(sentence):
-        words = word_tokenize(sentence)
-        meaningful_words = [str(w)\
-            if w not in stop_words else ''\
-            for w in words]
-        return ' '.join(meaningful_words)
-    
-    return input_col.map(remove, na_action='ignore')
+stop_words = set(stopwords.words("english"))
+
+def replace_fractions_with_decimals(s):
+    def add_nums(m):
+        return str(float(int(m.group(1)) + float(m.group(2)) / float(m.group(3))))
+    new_s = re.sub(r'(\d+)\s+(\d+)/(\d+)', add_nums, s)
+    new_s = re.sub(r'(\d+)/(\d+)', lambda m: str(float(m.group(1)) / float(m.group(2))), new_s)
+    return new_s
+
+def remove_stop_words(sentence):
+    words = word_tokenize(sentence)
+    meaningful_words = [str(w)\
+        if w not in stop_words else ''\
+        for w in words]
+    return ' '.join(meaningful_words)
 
 def get_processed_data():
+    """
+        * Remove stop words from input and columns
+            - rename 'input' to 'original_input'
+            - 'input' is now the processed input
+        * 
+    """
     file_path = './data/nyt-ingredients-snapshot-2015.csv'
     df_original = pd.read_csv(file_path)
 
@@ -23,11 +34,15 @@ def get_processed_data():
     df_original = df_original[df_original.input.notna()]
 
     # Transform Data
-    df_original.loc[df_original.unit == 'small pinch', 'unit'] = 'pinch'
+    # df_original.loc[df_original.unit == 'small pinch', 'unit'] = 'pinch'
 
     # Remove stop words from input and comment
-    df_original['input'] = remove_stop_words(df_original.input)
-    df_original['comment'] = remove_stop_words(df_original.comment)
+    df_original['input'] = df_original.input.map(remove_stop_words, na_action='ignore')
+    df_original['comment'] = df_original.comment.map(remove_stop_words, na_action='ignore')
+
+    # Replace Fractions with Decimals
+    df_original['input'] = df_original.input.map(replace_fractions_with_decimals)
+    df_original['comment'] = df_original.comment.map(replace_fractions_with_decimals, na_action='ignore')
 
     # Lemmatize name and units
 
@@ -41,3 +56,10 @@ def get_processed_data():
     df_new['comment'] = df_original.comment.str.lower()
 
     return df_new
+
+
+if __name__ == '__main__':
+    df_processed = get_processed_data()
+    df_processed.to_pickle('./data/nyt-data.pickle')
+
+    # df = pd.read_csv('./data/nyt-ingredients-snapshot-2015.csv')
